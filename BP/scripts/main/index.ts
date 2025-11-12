519b96f890d30889d3b285009208e6cc28976f26
ma27inranma <125137485+ma27inranma@users.noreply.github.com>
2025-11-12 19:58:34 +0900

    chore: build

---
+++ BP/scripts/main/index.ts
@@ -0,0 +1,5 @@
+export * from './entity/entity';
+export * from './colllision_box/collision_box';
+export * from './colllision_box/aabb';
+export * from './colllision_box/union';
+export * from './vec/vector3';
---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['BP/scripts/main/index.ts'],
+  format: ['esm'],
+  dts: true,
+  minify: true,
+  sourcemap: true,
+  clean: true,
+  outDir: 'BP/scripts/dist',
+  outExtension: ({ format }) => ({
+    js: '.js',
+    dts: '.d.ts'
+  })
+})
